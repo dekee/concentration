bbd5c8b201dda7a5a77b073648e97a77312fc0bd
Derrick Johnson <johnson.derrick@gmail.com>
2024-01-08 13:03:00 -0800

    Initial Commit

---
+++ concentration/concentrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  concentrationApp.swift
+//  concentration
+//
+//  Created by Derrick Johnson on 1/8/24.
+//
+
+import SwiftUI
+
+@main
+struct concentrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
